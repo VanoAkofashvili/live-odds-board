e130d9fd60e68af3333547ad17894e8f8b24d9f0
Vaniko Akopashvili <vaniko.akopashvili@makingscience.com>
2025-05-19 13:14:19 +0400

    Remove sorting

--- src/mocks/getInitialMatches.ts
+++ src/mocks/getInitialMatches.ts
@@ -5,9 +5,5 @@ import { getRandomMatch } from "./getRandomMatch";
 export const getInitialMatches = (
   count: number = AppConfig.MATCHES_COUNT
 ): Match[] => {
-  return Array.from({ length: count })
-    .map(getRandomMatch)
-    .sort((a, b) => {
-      return +(b.startTime as Date) - +(a.startTime as Date);
-    });
+  return Array.from({ length: count }).map(getRandomMatch);
 };
